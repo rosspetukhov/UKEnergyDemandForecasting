f0842ddeff025dadcfe71c64e144a7a6476d2f16
Ross Pietukhov <rospetukhov@gmail.com>
2025-05-24 10:55:00 +0100

    develop model and forecast with blob connection

--- model_development/train_model.py
+++ model_development/train_model.py
@@ -2,8 +2,6 @@ import pandas as pd
 import os
 from sklearn.linear_model import RidgeCV
 from sklearn.model_selection import train_test_split
-import numpy as np
-import matplotlib.pyplot as plt
 import pickle
 
 # Get path of the current script
@@ -46,32 +44,6 @@ model = RidgeCV()
 model.fit(X_train, y_train)
 
 # Save the model to a file
-with open('model_development/model.pkl', 'wb') as f:
+with open('model_development/ridge_model_24may.pkl', 'wb') as f:
     pickle.dump(model, f)
 
-# Forecast next 48 half-hour periods 
-last_datetime = df['datetime'].iloc[-1]
-future_preds = []
-recent_values = df.copy()
-
-for i in range(1, 49):  
-    future_time = last_datetime + pd.Timedelta(minutes=30 * i)
-    settlement_period = ((future_time.hour * 60 + future_time.minute) // 30) + 1
-
-    month = future_time.month
-    dayofweek = future_time.weekday()
-    lag_1 = future_preds[-1] if len(future_preds) > 0 else recent_values.iloc[-1]['ND']
-    lag_48 = recent_values.iloc[-48 + i]['ND'] if len(recent_values) >= 48 else lag_1
-
-    X_future = pd.DataFrame([[settlement_period, month, dayofweek, lag_1, lag_48]], columns=features)
-    pred = model.predict(X_future)[0]
-    future_preds.append(pred)
-
-# Plot the forecast
-plt.plot(range(1, 49), future_preds, marker='o')
-plt.title('Next 24-Hour Forecast (48 half-hour periods)')
-plt.xlabel('Half-Hour Period Ahead')
-plt.ylabel('Predicted ND')
-plt.grid(True)
-plt.tight_layout()
-plt.show()
